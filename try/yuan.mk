af887e9a677c0664db40c576ec9c5de80d89405c
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-03-24 12:36:36 -0500

    Revert "The version ran on HPC."
    This reverts commit a2e463571b7e622822986f4f1648c1f0f5c261a4.

--- try/yuan.mk
+++ try/yuan.mk
@@ -1,16 +1,13 @@
 
 all: yuan
 
-yuan:earthquake.o station.o yuan.o
+yuan: human.o yuan.o
+	g++ -Wall -o yuan human.o yuan.o
 
-yuan: earthquake.o station.o yuan.o
-	g++ -Wall -o yuan earthquake.o station.o yuan.o
 
-earthquake.o: earthquake.cpp
-	g++ -Wall -c earthquake.cpp
 
-station.o: station.cpp
-	g++ -Wall -c station.cpp
+human.o: human.cpp
+	g++ -Wall -c human.cpp
 
 yuan.o: yuan.cpp
 	g++ -Wall -c yuan.cpp
